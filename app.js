9ce746555c1c4cbd7aaee91122df1bcae82bd9da
bud aminof <budaminof@gmail.com>
2016-05-04 11:04:45 -0600

    piglatin for pokemon ! because we need to practice javascript. apparently

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-angular.module('pokemon', []);
+angular.module('pokemon', [])
+.filter('pigLatin', function(){
+   return function (input) {
+  var result = [];
+  var words = input.split(" ");
+
+  words.forEach(function(e){
+    var ending = ((e.split("").shift())) + 'ay';
+  var arr = e.split("");
+  arr.splice(0,1);
+  var chopped = arr.join("");
+  result.push(chopped + ending);
+  });
+ var newStr = result.join(" ");
+ return newStr;
+   };
+});
